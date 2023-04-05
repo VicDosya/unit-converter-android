f43ce236b098706ca95ab4cad741d570d0687500
dosya <vicdosya@gmail.com>
2023-04-05 13:36:45 +0300

    Add conversion logic to each card

--- app/src/main/java/com/example/convertorapp/activities/CalculateActivity.kt
+++ app/src/main/java/com/example/convertorapp/activities/CalculateActivity.kt
@@ -1,15 +1,19 @@
 package com.example.convertorapp.activities
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.RelativeLayout
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.convertorapp.R
-import com.example.convertorapp.data.getDropdownItems
+import com.example.convertorapp.data.*
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 
@@ -19,17 +23,25 @@ class CalculateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculate)
 
-        //Enable the Up button in the Action Bar
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //Views variables
+        //Relative Layout View
+        val calculateLayout = findViewById<RelativeLayout>(R.id.calculate_layout)
+        //Linear Layout View
+        val linearLayout = findViewById<LinearLayout>(R.id.unit_linear_layout)
+        //Number input layout + input value
+        val textInputLayout = findViewById<TextInputLayout>(R.id.ti_number_input)
+        val numberInput = findViewById<TextInputEditText>(R.id.et_number_input)
+        //Dropdown menu layout + input value
+        val optionsField = findViewById<TextInputLayout>(R.id.ti_options_field)
+        val chosenOption = findViewById<AutoCompleteTextView>(R.id.tv_auto_complete)
+        //Button View
+        val convertButton = findViewById<Button>(R.id.convert_button)
 
         //Get all extras from all the Activities
         val selectedCard = intent.getStringExtra("cardTitle")
 
-        //Assign cardTitle to the Action Bar title (if it exists)
-        supportActionBar?.title = selectedCard
 
         //Remove all inputs focus when clicked aside from the input fields
-        val calculateLayout = findViewById<RelativeLayout>(R.id.calculate_layout)
         calculateLayout.setOnClickListener {
             //Hide keyboard and clear focus
             val inputMethodManager =
@@ -39,13 +51,143 @@ class CalculateActivity : AppCompatActivity() {
         }
 
         //Drop down unit options
-        val optionsField = findViewById<TextInputLayout>(R.id.ti_options_field)
         if (selectedCard != null) {
             val unitsList = getDropdownItems(selectedCard)
             val adapter = ArrayAdapter(this, R.layout.dropdown_unit, unitsList)
             (optionsField.editText as? AutoCompleteTextView)?.setAdapter(adapter)
         }
 
+        //Set toolbar function with its selected card title
+        setToolBar(selectedCard)
+
+        //Convert button click listener
+        convertButton.setOnClickListener {
+            //If number input is empty, send layout error
+            if (numberInput.text.isNullOrEmpty()) {
+                textInputLayout.error = "Fill in a number"
+                //if no unit has been chosen at the dropbox layout, send layout error
+            } else if (chosenOption.text.isNullOrEmpty()) {
+                optionsField.error = "Choose a unit"
+                //Send conversion data
+            } else {
+                //Remove error lines (in case they exist)
+                textInputLayout.isErrorEnabled = false
+                optionsField.isErrorEnabled = false
+                //Remove all current converted TextViews
+                linearLayout.removeAllViewsInLayout()
+                //Get input number value
+                val number = numberInput.text.toString().toDoubleOrNull()
+                //Handle conversion for each card
+                when (selectedCard) {
+                    "Length" -> {
+                        //Values variables
+                        val conversionValues = LengthConversionMap[chosenOption.text.toString()]
+                        val meterValue = number?.times(conversionValues!!.meter)
+                        val centimeterValue = number?.times(conversionValues!!.centimeter)
+                        val inchValue = number?.times(conversionValues!!.inch)
+                        val footValue = number?.times(conversionValues!!.foot)
+                        //Display Data
+                        addTextViews(linearLayout, "Meter", meterValue)
+                        addTextViews(linearLayout, "Centimeter", centimeterValue)
+                        addTextViews(linearLayout, "Inch", inchValue)
+                        addTextViews(linearLayout, "Foot", footValue)
+                    }
+                    "Area" -> {
+                        //Values variables
+                        val conversionValues = AreaConversionMap[chosenOption.text.toString()]
+                        val sqrMeterValue = number?.times(conversionValues!!.sqrMeter)
+                        val sqrCentimeterValue = number?.times(conversionValues!!.sqrCentimeter)
+                        val sqrInchValue = number?.times(conversionValues!!.sqrInch)
+                        val sqrFootValue = number?.times(conversionValues!!.sqrFoot)
+                        //Display Data
+                        addTextViews(linearLayout, "Square Meter", sqrMeterValue)
+                        addTextViews(linearLayout, "Square Centimeter", sqrCentimeterValue)
+                        addTextViews(linearLayout, "Square Inch", sqrInchValue)
+                        addTextViews(linearLayout, "Square Foot", sqrFootValue)
+                    }
+                    "Volume" -> {
+                        //Values variables
+                        val conversionValues = VolumeConversionMap[chosenOption.text.toString()]
+                        val cubMeterValue = number?.times(conversionValues!!.cubMeter)
+                        val cubCentimeterValue = number?.times(conversionValues!!.cubCentimeter)
+                        val cubInchValue = number?.times(conversionValues!!.cubInch)
+                        val cubFootValue = number?.times(conversionValues!!.cubFoot)
+                        //Display Data
+                        addTextViews(linearLayout, "Cubic Meter", cubMeterValue)
+                        addTextViews(linearLayout, "Cubic Centimeter", cubCentimeterValue)
+                        addTextViews(linearLayout, "Cubic Inch", cubInchValue)
+                        addTextViews(linearLayout, "Cubic Foot", cubFootValue)
+                    }
+                    "Mass" -> {
+                        //Values variables
+                        val conversionValues = MassConversionMap[chosenOption.text.toString()]
+                        val kilogramValue = number?.times(conversionValues!!.kilogram)
+                        val gramValue = number?.times(conversionValues!!.gram)
+                        val ounceValue = number?.times(conversionValues!!.ounce)
+                        val poundValue = number?.times(conversionValues!!.pound)
+                        //Display Data
+                        addTextViews(linearLayout, "Kilogram", kilogramValue)
+                        addTextViews(linearLayout, "Gram", gramValue)
+                        addTextViews(linearLayout, "Ounce", ounceValue)
+                        addTextViews(linearLayout, "Pound", poundValue)
+                    }
+                    "Weight" -> {
+                        //Values variables
+                        val conversionValues = WeightConversionMap[chosenOption.text.toString()]
+                        val newtonValue = number?.times(conversionValues!!.newton)
+                        val kilopondValue = number?.times(conversionValues!!.kilopond)
+                        val poundForceValue = number?.times(conversionValues!!.poundForce)
+                        //Display Data
+                        addTextViews(linearLayout, "Newton", newtonValue)
+                        addTextViews(linearLayout, "Kilopond", kilopondValue)
+                        addTextViews(linearLayout, "Pound-Force", poundForceValue)
+                    }
+                    "Time" -> {
+                        //Values variables
+                        val conversionValues = TimeConversionMap[chosenOption.text.toString()]
+                        val secondValue = number?.times(conversionValues!!.second)
+                        val minuteValue = number?.times(conversionValues!!.minute)
+                        val hourValue = number?.times(conversionValues!!.hour)
+                        val dayValue = number?.times(conversionValues!!.day)
+                        val weekValue = number?.times(conversionValues!!.week)
+                        val monthValue = number?.times(conversionValues!!.month)
+                        val yearValue = number?.times(conversionValues!!.year)
+                        //Display Data
+                        addTextViews(linearLayout, "Second", secondValue)
+                        addTextViews(linearLayout, "Minute", minuteValue)
+                        addTextViews(linearLayout, "Hour", hourValue)
+                        addTextViews(linearLayout, "Day", dayValue)
+                        addTextViews(linearLayout, "Week", weekValue)
+                        addTextViews(linearLayout, "Month", monthValue)
+                        addTextViews(linearLayout, "Year", yearValue)
+                    }
+                    "Electric current" -> {
+                        //Values variables
+                        val conversionValues =
+                            ElectricCurrentConversionMap[chosenOption.text.toString()]
+                        val ampereValue = number?.times(conversionValues!!.ampere)
+                        val milliAmpereValue = number?.times(conversionValues!!.milliAmpere)
+                        val microAmpereValue = number?.times(conversionValues!!.microAmpere)
+                        //Display Data
+                        addTextViews(linearLayout, "Ampere", ampereValue)
+                        addTextViews(linearLayout, "Milliampere", milliAmpereValue)
+                        addTextViews(linearLayout, "Microampere", microAmpereValue)
+                    }
+                    "Temperature" -> {
+                        //Values variables
+                        val conversionValues =
+                            TemperatureConversionMap[chosenOption.text.toString()]
+                        val celsiusValue = conversionValues?.celsius?.invoke(number!!)
+                        val fahrenheitValue = conversionValues?.fahrenheit?.invoke(number!!)
+                        val kelvinValue = conversionValues?.kelvin?.invoke(number!!)
+                        //Display Data
+                        addTextViews(linearLayout, "Celsius", celsiusValue)
+                        addTextViews(linearLayout, "Fahrenheit", fahrenheitValue)
+                        addTextViews(linearLayout, "Kelvin", kelvinValue)
+                    }
+                }
+            }
+        }
     }
 
     //Enable the back button to finish the CalculateActivity
@@ -56,5 +198,34 @@ class CalculateActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    //Function to add new TextViews in the LinearLayout
+    private fun addTextViews(linearLayout: LinearLayout, unit: String, unitValue: Double?) {
+        val textView = TextView(this)
+        textView.text = getString(R.string.unit_value_template, unit, unitValue)
+        linearLayout.addView(textView)
+    }
+
+    //Assign cardTitle to the Action Bar title (if it exists)
+    //Set its text color to black
+    //Include back arrow button
+    private fun setToolBar(selectedCard: String?) {
+        //Enable the Up button in the Action Bar
+        supportActionBar?.apply {
+            setBackgroundDrawable(ColorDrawable(Color.WHITE))
+            setHomeAsUpIndicator(com.google.android.material.R.drawable.ic_arrow_back_black_24)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        val spannableString = SpannableString(selectedCard)
+        spannableString.setSpan(
+            ForegroundColorSpan(Color.BLACK),
+            0,
+            selectedCard!!.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.title = spannableString
     }
 }
--- app/src/main/java/com/example/convertorapp/data/CardRVModal.kt
+++ app/src/main/java/com/example/convertorapp/data/CardRVModal.kt
@@ -31,9 +31,142 @@ fun getDropdownItems(cardTitle: String): List<String> {
         "Volume" -> listOf("Cubic Meter", "Cubic Centimeter", "Cubic Inch", "Cubic Foot")
         "Mass" -> listOf("Kilogram", "Gram", "Ounce", "Pound")
         "Weight" -> listOf("Newton", "Kilopond", "Pound-Force")
-        "Time" -> listOf("Second", "Minute", "Hour", "Day")
+        "Time" -> listOf("Second", "Minute", "Hour", "Day", "Week", "Month", "Year")
         "Electric current" -> listOf("Ampere", "Milliampere", "Microampere")
         "Temperature" -> listOf("Celsius", "Fahrenheit", "Kelvin")
         else -> emptyList()
     }
 }
+
+//Conversion data
+//LENGTH
+data class LengthConversionValues(
+    val meter: Double,
+    val centimeter: Double,
+    val inch: Double,
+    val foot: Double,
+)
+//LENGTH CONVERSION FORMULA
+val LengthConversionMap = mapOf(
+    "Meter" to LengthConversionValues(1.0, 100.0, 39.37, 3.28),
+    "Centimeter" to LengthConversionValues(0.01, 1.0, 0.39, 0.03),
+    "Inch" to LengthConversionValues(0.0254, 2.54, 1.0, 0.083333),
+    "Foot" to LengthConversionValues(0.3048, 30.48, 12.0, 1.0),
+)
+
+//AREA
+data class AreaConversionValues(
+    val sqrMeter: Double,
+    val sqrCentimeter: Double,
+    val sqrInch: Double,
+    val sqrFoot: Double,
+)
+//AREA CONVERSION FORMULA
+val AreaConversionMap = mapOf(
+    "Square Meter" to AreaConversionValues(1.0, 10000.0, 1550.0031, 10.7639),
+    "Square Centimeter" to AreaConversionValues(0.0001, 1.0, 0.0016, 0.000010764),
+    "Square Inch" to AreaConversionValues(0.00064516, 6.4516, 1.0, 0.00694444),
+    "Square Foot" to AreaConversionValues(0.092903, 929.0304, 144.0, 1.0),
+)
+
+//VOLUME
+data class VolumeConversionValues(
+    val cubMeter: Double,
+    val cubCentimeter: Double,
+    val cubInch: Double,
+    val cubFoot: Double,
+)
+//VOLUME CONVERSION FORMULA
+val VolumeConversionMap = mapOf(
+    "Cubic Meter" to VolumeConversionValues(1.0, 1000000.0, 61023.7, 35.3147),
+    "Cubic Centimeter" to VolumeConversionValues(0.000001, 1.0, 0.0610237, 0.0000353147),
+    "Cubic Inch" to VolumeConversionValues(0.0000163871, 16.3871, 1.0, 0.000578704),
+    "Cubic Foot" to VolumeConversionValues(0.0283168, 28316.8, 1728.0, 1.0),
+)
+
+//MASS
+data class MassConversionValues(
+    val kilogram: Double,
+    val gram: Double,
+    val ounce: Double,
+    val pound: Double,
+)
+//MASS CONVERSION FORMULA
+val MassConversionMap = mapOf(
+    "Kilogram" to MassConversionValues(1.0, 1000.0, 35.274, 2.205),
+    "Gram" to MassConversionValues(0.001, 1.0, 0.0353, 0.0022),
+    "Ounce" to MassConversionValues(0.0283, 28.3495, 1.0, 0.0625),
+    "Pound" to MassConversionValues(0.4536, 453.592, 16.0, 1.0),
+)
+
+//WEIGHT
+data class WeightConversionValues(
+    val newton: Double,
+    val kilopond: Double,
+    val poundForce: Double
+)
+//WEIGHT CONVERSION FORMULA
+val WeightConversionMap = mapOf(
+    "Newton" to WeightConversionValues(1.0, 0.10197, 0.22481),
+    "Kilopond" to WeightConversionValues(9.80665, 1.0, 2.20462),
+    "Pound-Force" to WeightConversionValues(4.44822, 0.45359, 1.0),
+)
+
+//TIME
+data class TimeConversionValues(
+    val second: Double,
+    val minute: Double,
+    val hour: Double,
+    val day: Double,
+    val week: Double,
+    val month: Double,
+    val year: Double,
+)
+//TIME CONVERSION FORMULA
+val TimeConversionMap = mapOf(
+    "Second" to TimeConversionValues(1.0, 0.0166667, 0.000277778, 0.0000115741, 0.00000165344, 0.000000380517, 0.0000000316881),
+    "Minute" to TimeConversionValues(60.0, 1.0, 0.0166667, 0.000694444, 0.0000992063, 0.0000228311, 0.00000190133),
+    "Hour" to TimeConversionValues(3600.0, 60.0, 1.0, 0.0416667, 0.00595238, 0.00136895, 0.00011408),
+    "Day" to TimeConversionValues(86400.0, 1440.0, 24.0, 1.0, 0.142857, 0.0328767, 0.00273973),
+    "Week" to TimeConversionValues(604800.0, 10080.0, 168.0, 7.0, 1.0, 0.229985, 0.0191781),
+    "Month" to TimeConversionValues(2629800.0, 43830.0, 730.5, 30.4375, 4.34812, 1.0, 0.0833333),
+    "Year" to TimeConversionValues(31557600.0, 525960.0, 8760.0, 365.25, 52.1786, 12.0, 1.0)
+)
+
+//ELECTRIC CURRENT
+data class ElectricCurrentConversionValues(
+    val ampere: Double,
+    val milliAmpere: Double,
+    val microAmpere: Double,
+)
+//ELECTRIC CURRENT CONVERSION FORMULA
+val ElectricCurrentConversionMap = mapOf(
+    "Ampere" to ElectricCurrentConversionValues(1.0, 1000.0, 1000000.0),
+    "Milliampere" to ElectricCurrentConversionValues(0.001, 1.0, 1000.0),
+    "Microampere" to ElectricCurrentConversionValues(0.000001, 0.001, 1.0)
+)
+
+//TEMPERATURE
+data class TemperatureConversionValues(
+    val celsius: (Double) -> Double,
+    val fahrenheit: (Double) -> Double,
+    val kelvin: (Double) -> Double,
+)
+//TEMPERATURE CONVERSION FORMULA
+val TemperatureConversionMap = mapOf(
+    "Celsius" to TemperatureConversionValues(
+        celsius = { it },
+        fahrenheit = { it * 9.0 / 5.0 + 32.0 },
+        kelvin = { it + 273.15 }
+    ),
+    "Fahrenheit" to TemperatureConversionValues(
+        celsius = { (it - 32.0) * 5.0 / 9.0 },
+        fahrenheit = { it },
+        kelvin = { (it + 459.67) * 5.0 / 9.0 }
+    ),
+    "Kelvin" to TemperatureConversionValues(
+        celsius = { it - 273.15 },
+        fahrenheit = { it * 9.0 / 5.0 - 459.67 },
+        kelvin = { it }
+    )
+)
